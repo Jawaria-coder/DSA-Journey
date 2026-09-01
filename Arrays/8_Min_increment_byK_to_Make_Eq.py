c78f66fc77ed5cee14ddd4c15d869dc8e0204b9d
Jawaria-coder <jaru.jawaria@gmail.com>
2026-09-01 15:18:49 +0500

    Min incre By K to make Eq

---
+++ Arrays/8_Min_increment_byK_to_Make_Eq.py
@@ -0,0 +1,15 @@
+def minOps(self, arr, k):
+        # code here
+    maxVal = max(arr)
+    operations = 0
+    
+    for i in range(len(arr)):
+        if((maxVal - arr[i]) % k) != 0:
+            return -1
+    
+        operations += (maxVal - arr[i]) // k 
+        
+    return operations
+
+Time : O(n)
+Space: O(1)
