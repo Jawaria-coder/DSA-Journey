b970b04aef56355013a56db6adf19e265fbc37b4
Jawaria-coder <jaru.jawaria@gmail.com>
2026-09-02 12:23:38 +0500

    Duplicate within K distance SET

--- Arrays/10_Dupli_within_K_Distance.py
+++ Arrays/10_Dupli_within_K_Distance.py
@@ -9,3 +9,21 @@ def checkDuplicatesWithinK(self, arr: list[int], k: int) -> bool:
 
 # Time : O(n*k)
 # Space: O(1)
+
+### Set Approach
+def checkDuplicatesWithinKset(self, arr: list[int], k: int) -> bool:
+    seen = set()
+    
+    for i in range(len(arr)):
+        if arr[i] in seen:
+            return True
+        
+        seen.add(arr[i])
+        
+        if len(seen) > k:
+            seen.remove(arr[i-k])
+            
+    return False
+
+# Time : O(n)
+# Space: O(k)
