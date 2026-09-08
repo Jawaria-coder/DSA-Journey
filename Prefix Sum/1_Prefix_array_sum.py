9f3624eb867c06ac91647f1de73f61992234b537
Jawaria-coder <jaru.jawaria@gmail.com>
2026-09-08 18:52:53 +0500

    Pre Req Prefix Sum Array

---
+++ Prefix Sum/1_Prefix_array_sum.py
@@ -0,0 +1,18 @@
+# function to find the prefix sum array
+def prefSum(arr):
+    n = len(arr)
+    
+    # to store the prefix sum
+    prefixSum = [0] * n
+
+    # initialize the first element
+    prefixSum[0] = arr[0]
+
+    # Adding present element with previous element
+    for i in range(1, n):
+        prefixSum[i] = prefixSum[i - 1] + arr[i]
+    
+    return prefixSum
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
