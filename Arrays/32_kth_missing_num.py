8c3aff315ed9893eda8bcadb551b04a067d634b2
Jawaria-coder <jaru.jawaria@gmail.com>
2026-09-14 20:48:17 +0500

    kth missing +ve num in sorted array

---
+++ Arrays/32_kth_missing_num.py
@@ -0,0 +1,19 @@
+def kthMissing(arr, k):
+    lo = 0
+    hi = len(arr) - 1
+    res = len(arr) + k
+
+    # binary Search for index where 
+    # arr[i] > (i + k)
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if arr[mid] > mid + k:
+            res = mid + k
+            hi = mid - 1
+        else:
+            lo = mid + 1
+
+    return res
+
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
